8356b5ae4e9f6c719e392366236a6183156f1aa1
Erwin Koekoek <erwin@koekies.org>
2014-09-03 19:35:02 +0200

    initial commit

---
+++ l10n/en_GB.php
@@ -0,0 +1,15 @@
+<?php
+$TRANSLATIONS = array(
+"Back" => "Back",
+"Close" => "Close",
+"Home" => "Home",
+"Open" => "Open",
+"Save" => "Save",
+"Refresh" => "Refresh",
+"Select Image" => "Select Image",
+"Browse and select an image" => "Browse and select an image",
+"Keep Aspect Ratio" => "Keep Aspect Ratio",
+"Hour Symbol" => "h",
+"Select" => "Select"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
