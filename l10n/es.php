d8d41eae6c128abaee5f3756b5e72eb72418dece
kjuanman <juanma@kde.org.ar>
2014-09-04 19:14:57 -0300

    Create es.php
    spanish translation

---
+++ l10n/es.php
@@ -0,0 +1,15 @@
+<?php
+$TRANSLATIONS = array(
+"Back" => "Atrás",
+"Close" => "Cerrar",
+"Home" => "Inicio",
+"Open" => "Abrir",
+"Save" => "Guardar",
+"Refresh" => "Actualizar",
+"Select Image" => "Seleccionar imagen",
+"Browse and select an image" => "Explorar y seleccionar una imagen",
+"Keep Aspect Ratio" => "Mantener relación de aspecto",
+"Hour Symbol" => "h",
+"Select" => "Seleccionar"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
