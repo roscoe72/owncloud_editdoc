fe930d6ea20c31e06b19516d56f32b920ffbfcee
Erwin Koekoek <erwin@koekies.org>
2014-11-12 20:10:06 +0100

    Create fr.php - Thanks to Marc Abel

---
+++ l10n/fr.php
@@ -0,0 +1,15 @@
+<?php
+$TRANSLATIONS = array(
+"Back" => "Retour",
+"Close" => "Fermer",
+"Home" => "Racine",
+"Open" => "Ouvrir",
+"Save" => "Enregistrer",
+"Refresh" => "Rafraichir",
+"Select Image" => "Choisir une Image",
+"Browse and select an image" => "Parcourir et choisir une image",
+"Keep Aspect Ratio" => "Maintenir les proportions",
+"Hour Symbol" => "h",
+"Select" => "Choisir"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
